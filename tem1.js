8becbfad363a0404b1a2c1213a23da6583a59547
Nian <1046783533@qq.com>
2020-09-11 23:49:12 +0800

    init

---
+++ tem1.js
@@ -0,0 +1,12 @@
+function x() {
+    var allChildren = item3.parentNode.children
+    var array = {
+        length: 0
+    }
+    for (let i = 0; i < allChildren.length; i++) {
+        if (allChildren[i] !== item3) {
+            array[array.length] = allChildren[i]
+            array.length += 1
+        }
+    }
+}

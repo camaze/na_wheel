1e048592d28015b28b809e6dec6eacf16a5e5498
Nian <1046783533@qq.com>
2020-09-16 16:03:02 +0800

    before .one

---
+++ seamless.js
@@ -0,0 +1,41 @@
+setTimeout(() => {
+    $('.images>img:nth-child(1)').css({
+        transform: 'translatX(-100%)'
+    })
+    $('.images>img:nth-child(2)').css({
+        transform: 'translateX(-100%)'
+    })
+    $('.images>img:nth-child(1)').on('transitionend', function (e) {
+        $(e.currentTarget).addClass('right').css({
+            transform: 'none'
+        })
+    })
+}, 3000)
+
+setTimeout(() => {
+    $('.images>img:nth-child(2)').css({
+        transform: 'translatX(-200%)'
+    })
+    $('.images>img:nth-child(3)').css({
+        transform: 'translateX(-100%)'
+    })
+    $('.images>img:nth-child(2)').on('transitionend', function (e) {
+        $(e.currentTarget).addClass('right').css({
+            transform: 'none'
+        })
+    })
+}, 6000)
+
+setTimeout(() => {
+    $('.images>img:nth-child(3)').css({
+        transform: 'translatX(-200%)'
+    })
+    $('.images>img:nth-child(1)').css({
+        transform: 'translateX(-100%)'
+    })
+    $('.images>img:nth-child(3)').on('transitionend', function (e) {
+        $(e.currentTarget).addClass('right').css({
+            transform: 'none'
+        })
+    })
+}, 9000)

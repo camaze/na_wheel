9f9ca0534b27725a46553ab64d36ca39ba6d0347
Nian <1046783533@qq.com>
2020-09-21 17:56:25 +0800

    now auto carousel supported

--- blank.js
+++ blank.js
@@ -7,61 +7,61 @@
 //     console.log(a); // a => 10
 // }
 
-window.jQuery = function (nodeOrSelector) {
-    let nodes = {}
-    if (typeof nodeOrSelector === 'string') {
-        let temp = document.querySelectorAll(nodeOrSelector)
-        for (let i = 0; i < temp.length; i++) {
-            nodes[i] = temp[i]
-        }
-        nodes.length = temp.length
-        console.log(nodes)
-    } else if (nodeOrSelector instanceof Node) {
-        nodes = {
-            0: nodeOrSelector,
-            length: 1
-        }
-    }
-    nodes.addClass = function (classes) {
-        // 每个节点均加一个类
-        classes.forEach((value) => {
-            for (let i = 0; i < nodes.length; i++) {
-                nodes[i].classList.add(value)
-            }
-        });
-    }
-    nodes.getText = function () {
-        var texts = []
-        for (let i = 0; i < nodes.length; i++) {
-            texts.push(nodes[i].textContent)
-        }
-        return texts
-    }
-    nodes.setText = function (text) {
-        for (let i = 0; i < nodes.length; i++) {
-            nodes[i].textContent = text
-        }
-    }
-    nodes.text = function (text) {
-        if (text === undefined) {
-            // 如果是 node2.text()说明想要text
-            var texts = []
-            for (let i = 0; i < nodes.length; i++) {
-                texts.push(nodes[i].textContent)
-            }
-            return texts
-        } else {
-            // 如果是 node2.text('aaa') 说明想把文本内容设置为aaa
-            for (let i = 0; i < nodes.length; i++) {
-                nodes[i].textContent = text
-            }
-        }
-    }
-    return nodes
-}
-var node2 = jQuery('ul > li')
-node2.addClass(['blue'])
-node2.text('hi')
+// window.jQuery = function (nodeOrSelector) {
+//     let nodes = {}
+//     if (typeof nodeOrSelector === 'string') {
+//         let temp = document.querySelectorAll(nodeOrSelector)
+//         for (let i = 0; i < temp.length; i++) {
+//             nodes[i] = temp[i]
+//         }
+//         nodes.length = temp.length
+//         console.log(nodes)
+//     } else if (nodeOrSelector instanceof Node) {
+//         nodes = {
+//             0: nodeOrSelector,
+//             length: 1
+//         }
+//     }
+//     nodes.addClass = function (classes) {
+//         // 每个节点均加一个类
+//         classes.forEach((value) => {
+//             for (let i = 0; i < nodes.length; i++) {
+//                 nodes[i].classList.add(value)
+//             }
+//         });
+//     }
+//     nodes.getText = function () {
+//         var texts = []
+//         for (let i = 0; i < nodes.length; i++) {
+//             texts.push(nodes[i].textContent)
+//         }
+//         return texts
+//     }
+//     nodes.setText = function (text) {
+//         for (let i = 0; i < nodes.length; i++) {
+//             nodes[i].textContent = text
+//         }
+//     }
+//     nodes.text = function (text) {
+//         if (text === undefined) {
+//             // 如果是 node2.text()说明想要text
+//             var texts = []
+//             for (let i = 0; i < nodes.length; i++) {
+//                 texts.push(nodes[i].textContent)
+//             }
+//             return texts
+//         } else {
+//             // 如果是 node2.text('aaa') 说明想把文本内容设置为aaa
+//             for (let i = 0; i < nodes.length; i++) {
+//                 nodes[i].textContent = text
+//             }
+//         }
+//     }
+//     return nodes
+// }
+// var node2 = jQuery('ul > li')
+// node2.addClass(['blue'])
+// node2.text('hi')
 
 
 
@@ -74,3 +74,18 @@ node2.text('hi')
       <li id="item5">选项5</li>
     </ul> */
 }
+
+//在函数外使用let关键字声明变量test_var
+let test_var = "函数外的test_var";
+//定义函数testFun
+function testFun() {
+    //打印输出test_var
+    console.log(test_var);
+    //在函数内使用let关键字声明变量test_var
+    // let test_var = "函数内的test_var"
+    //打印输出test_var
+    test_var = 'aaa'
+    console.log(test_var);
+}
+//函数执行
+testFun();
--- ra_seamless.js
+++ ra_seamless.js
@@ -2,46 +2,123 @@
 let $buttons = $('#buttonWrapper>button')
 let $slides = $('#slides')
 let $images = $slides.children('img')
-let $firstCopy = $images.eq(0).clone(true)
-let $lastCopy = $images.eq($images.length - 1).clone(true)
+let current = 0
 
-$slides.append($firstCopy)
-$slides.prepend($lastCopy)
+// 1 做些假图片
+makeFakeSlides()
 
+// 2 确定初始位置
 $slides.hide().offset()
 $slides.css({
     transform: 'translateX(-300px)'
 })
 $slides.show()
 
+// 3 绑定按键事件
+bindEvents()
+$(next).on('click', function () {
+    goToSlide(current + 1)
+})
+
+$(previous).on('click', function () {
+    goToSlide(current - 1)
+})
+
+let timer = setInterval(function () {
+    goToSlide(current + 1)
+}, 1000)
+
+$('.container').on('mouseenter', function () {
+    window.clearInterval(timer)
+}).on('mouseleave', function () {
+    timer = setInterval(function () {
+        goToSlide(current + 1)
+    }, 1000)
+})
 
 
-let current = 0
+/*  below are functions    */
+function makeFakeSlides() {
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length - 1).clone(true)
+    $slides.append($firstCopy) // 第一张放最后
+    $slides.prepend($lastCopy) // 最后一张放最前
+}
 
-$buttons.eq(0).on('click', function () {
-    if (current === 2) {
-        console.log('说明是从最后一张到第一张')
+function bindEvents() {
+    $('#buttonWrapper').on('click', 'button', function (e) {
+        let $button = $(e.currentTarget)
+        let index = $button.index()
+        goToSlide(index)
+    })
+}
+
+function goToSlide(index) {
+    if (index > $buttons.length - 1) {
+        index = 0
+    } else if (index < 0) {
+        index = $buttons.length - 1
+    }
+    if (current === $buttons.length - 1 && index === 0) {
+        // 最后一张到第一张
         $slides.css({
-            transform: 'translateX(-1200px)'
+            transform: `translateX(${-($buttons.length + 1)*300}px)`
+        }).one('transitionend', function () {
+            $slides.hide().offset()
+            $slides.css({
+                transform: `translateX(${-(index+1)*300}px)`
+            }).show()
+        })
+    } else if (current === 0 && index === $buttons.length - 1) {
+        //第一张到最后一张
+        $slides.css({
+            transform: `translateX(0px)`
+        }).one('transitionend', function () {
+            $slides.hide().offset()
+            $slides.css({
+                transform: `translateX(${-(index+1)*300}px)`
+            }).show()
         })
     } else {
         $slides.css({
-            transform: 'translateX(-300px)'
+            transform: `translateX(${-(index+1)*300}px)`
         })
     }
-    current = 0
-})
+    current = index
+}
 
-$buttons.eq(1).on('click', function () {
-    $slides.css({
-        transform: 'translateX(-600px)'
-    })
-    current = 1
-})
 
-$buttons.eq(2).on('click', function () {
-    $slides.css({
-        transform: 'translateX(-900px)'
-    })
-    current = 2
-})
+// $buttons.eq(0).on('click', function () {
+//     if (current === 2) {
+//         console.log('说明是从最后一张到第一张')
+//         $slides.css({
+//             transform: 'translateX(-1200px)'
+//         }).one('transitionend', function () {
+//             // 变完之后回到真实的第一张
+//             $slides.hide().offset()
+//             $slides.css({
+//                 transform: 'translateX(-300px)'
+//             }).show()
+//         })
+//     } else {
+//         $slides.css({
+//             transform: 'translateX(-300px)'
+//         })
+//     }
+//     current = 0
+// })
+
+// $buttons.eq(1).on('click', function () {
+//     $slides.css({
+//         transform: 'translateX(-600px)'
+//     })
+//     current = 1
+// })
+
+// $buttons.eq(2).on('click', function () {
+//     $slides.css({
+//         transform: 'translateX(-900px)'
+//     })
+//     current = 2
+// })
+// }
